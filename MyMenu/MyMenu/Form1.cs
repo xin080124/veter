4c28a3a72d0052c9e974b3571712a964686cf792
nancyUnitec <xin080124@hotmail.com>
2017-05-19 21:23:37 +1200

    add my menu exercise

---
+++ MyMenu/MyMenu/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace MyMenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItem4_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine(" confirm menuItem4_Click");
+        }
+    }
+}
