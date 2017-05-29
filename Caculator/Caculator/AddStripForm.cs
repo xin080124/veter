e24ab25f7fadadc1df37fb356888796feefbf935
yx <hxy820718@163.com>
2017-05-29 20:30:25 +1200

    add layout

--- Caculator/Caculator/AddStripForm.cs
+++ Caculator/Caculator/AddStripForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,55 @@ namespace Caculator
 {
     public partial class AddStripForm : Form
     {
+        private bool hasOp;
+
         public AddStripForm()
         {
             InitializeComponent();
+            hasOp = false;
         }
 
         private void jkhkToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //lstDisplay;
+        }
 
+   
+
+
+        private void AddStripForm_KeyDown(object sender, KeyEventArgs e)
+        {
+          
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /*
+            if (e.KeyCode == Keys.Return)
+            {
+                Debug.WriteLine("enter");
+            }
+            */
+            /*
+            int kc = (int)e.KeyCode;
+            Debug.WriteLine(kc);
+            */
+            
+            if (((int)(e.KeyCode) < 122)&&((int)(e.KeyCode) > 65))
+            {
+                Debug.WriteLine("forbidden");
+                MessageBox.Show("Please fill the year box with number and operator.", "Error");
+                textBox1.Text = "";
+            }
+            else
+            {
+                Debug.WriteLine("ok");
+                if (hasOp)
+                {
+
+                }
+            }
+            
         }
     }
 }
